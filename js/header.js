521c5f972d2fd9c121bb7888896d2eb9295f8476
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2023-09-29 11:18:50 +0200

    update header v.1

--- js/header.js
+++ js/header.js
@@ -58,32 +58,33 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 */
 
-/* LES CONST POUR ACTIONNER BUTTON
-const signupButton = document.getElementById("log-in-button");
-const formSignUp = document.getElementById("idBtnLogIn");
+/*LES CONST POUR ACTIONNER BUTTON*/
+const signupButton = document.getElementById("sign-up-button");
+const formSignUp = document.getElementById("idBtnSignUp");
 const loginButton = document.getElementById("log-in-button");
 const formLogIn = document.getElementById("idBtnLogIn");
 const closeButton = document.getElementById("closeButton");
 
 // ---------FUNCTION APPARITION FORM SIGN-UP--------------//
 
-// BUTTON CLICKER, FAIT APP LE FORM
-loginButton.addEventListener("click", function () {
-  formLogIn.style.display = "block";
+// BUTTON CLICKER, FAIT APP LE FORM//
+
+signupButton.addEventListener("click", function () {
+  formSignUp.style.display = "block";
+  console.log("apparition formulaire sign up");
 });
 
 // QUAND LE BUTTON FERMER CLICK, CACHE LE FORM
 closeButton.addEventListener("click", function () {
-  formLogIn.style.display = "none";
+  formSignUp.style.display = "none";
 });
 
 // CLICK AUTRE PART QUE LA CROIX --> FERME LE FORM
 window.addEventListener("click", function (event) {
-  if (event.target === formLogIn) {
-    formLogIn.style.display = "none";
+  if (event.target === formSignUp) {
+    formSignUp.style.display = "none";
   }
 });
-*/
 
 // ---------FUNCTION APPARITION FORM LOG-IN--------------//
 

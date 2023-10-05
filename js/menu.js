2ba14b0b689a327c05869297de76cd760e52791b
Alexis_Lienard <lienard.alexis@gmail.com>
2023-10-05 15:10:42 +0200

    last version

--- js/menu.js
+++ js/menu.js
@@ -1,131 +1,131 @@
-console.log("hello");
+console.log("Hello");
 
-/* CREATING ELEMENTS  */
+/* CREATING ELEMENTS */
 
-function order(restaurantList) {
-  restaurantList.forEach((list) => {
-  
-    //Parent div
-    const main = document.querySelector(".menu__container");
+function fillRestaurant(array) {
 
-    //wrapper of the 2 cards : restaurant and map
-    const portfolio = document.createElement("div");
-    portfolio.classList.add("wrapper");
-    portfolio.classList.add("resizeable");
-    main.appendChild(portfolio);
+// recuperer tous les ARTICLES contenus dans #menu__container
+const currentElements = document.querySelectorAll("#menu__container > article");
 
-    //Restaurant card
-    const menuCardRestaurant = document.createElement("article");
-    menuCardRestaurant.classList.add("menu__cardRestaurant");
-    portfolio.appendChild(menuCardRestaurant);
+  // pour chaque element dedans, le supprimer
+currentElements.forEach (function deleteElements(element) {
+    element.remove();
+  }
+);
+  // ajout des elements contenus dans le tableau array
+array.forEach((eRestaurant) => {
 
-    //Map card
-    const menuCardMap = document.createElement("article");
-    menuCardMap.classList.add("menu__cardMap");
-    menuCardMap.classList.add("hidden");
-    menuCardMap.style.backgroundImage = `url(${list.images.map})`
-    portfolio.appendChild(menuCardMap);
+//Parent div
+const main = document.querySelector(".container");
 
-    //Top of the restaurant card
-    const menuCardPicture = document.createElement("div");
-    menuCardPicture.classList.add("menu__card-picture");
-    menuCardPicture.style.backgroundImage = `url(${list.images.main})`
-    menuCardRestaurant.appendChild(menuCardPicture);
+//wrapper of the 2 cards : restaurant and map
+const article = document.createElement("article");
+article.classList.add("wrapper");
+main.appendChild(article);
 
-    //Bottom of the restaurant card
-    const menuCardBody = document.createElement("div");
-    menuCardBody.classList.add("menu__card-body");
-    menuCardRestaurant.appendChild(menuCardBody);
+//Restaurant card
+const mainRestaurant = document.createElement("main");
+mainRestaurant.classList.add("product","restaurant");
+article.appendChild(mainRestaurant);
 
-    //Container of the titles of the restaurant card
-    const cardBodyTitle = document.createElement("div");
-    cardBodyTitle.classList.add("menu__card-body-title");
-    menuCardBody.appendChild(cardBodyTitle);
-  
-    //Name of the restaurant
-    const restaurantName = document.createElement("h2");
-    restaurantName.classList.add("restaurant-name");
-    restaurantName.innerHTML = list.name;
-    cardBodyTitle.appendChild(restaurantName);
+//Map card
+const mainMap = document.createElement("main");
+mainMap.classList.add("product","map");
+article.appendChild(mainMap);
 
-    //Type of the restaurant
-    const restaurantType = document.createElement("h3");
-    restaurantType.classList.add("restaurant-type");
-    restaurantType.innerHTML = list.type;
-    cardBodyTitle.appendChild(restaurantType);
-    
-    //Container of the values of the restaurant card
-    const cardBodyValue = document.createElement("div");
-    cardBodyValue.classList.add("menu__card-body-value");
-    menuCardBody.appendChild(cardBodyValue);
+//Restaurant img
+const restaurantImg = document.createElement("picture");
+restaurantImg.classList.add("background");
+restaurantImg.style.backgroundImage = `url(${eRestaurant.images.main})`
+mainRestaurant.appendChild(restaurantImg);
 
-    //Popularity value
-    const popularityValue = document.createElement("h3");
-    popularityValue.classList.add("popularityValue");
-    popularityValue.innerHTML = `${"⭐️".repeat(list.popularity)}`;
-    cardBodyValue.appendChild(popularityValue);
+//Restaurant infos
+const restaurantInfos = document.createElement("div");
+restaurantInfos.classList.add("restaurant-infos");
+mainRestaurant.appendChild(restaurantInfos);
 
-    //Distance value
-    const distanceValue = document.createElement("h3");
-    distanceValue.classList.add("card-distance");
-    distanceValue.innerHTML = `${list.distance} min`;
-    cardBodyValue.appendChild(distanceValue);
+//Restaurant title
+const restaurantTitle = document.createElement("h1");
+restaurantTitle.classList.add("title");
+restaurantTitle.innerHTML = eRestaurant.name;
+restaurantInfos.appendChild(restaurantTitle);
 
-    //Price value
-    const priceValue = document.createElement("h3"); 
-    priceValue.classList.add("card-price");
-    priceValue.innerHTML = `${"€".repeat(list.price)}`;
-    cardBodyValue.appendChild(priceValue);
+//Restaurant type
+const restaurantType = document.createElement("h2");
+restaurantType.classList.add("type");
+restaurantType.innerHTML = eRestaurant.type;
+restaurantInfos.appendChild(restaurantType);
 
-  });
+//Restaurant description
+const restaurantDescription = document.createElement("p");
+restaurantDescription.classList.add("description");
+restaurantInfos.appendChild(restaurantDescription);
+
+//Description span popularity
+const descriptionSpanStar = document.createElement("span");
+descriptionSpanStar.innerHTML = `${"⭐️".repeat(eRestaurant.popularity)}`;
+restaurantDescription.appendChild(descriptionSpanStar);
+//Description span distance
+const descriptionSpanDistance = document.createElement("span");
+descriptionSpanDistance.innerHTML = `${eRestaurant.distance} min`;
+restaurantDescription.appendChild(descriptionSpanDistance);
+//Description span price
+const descriptionSpanPrice = document.createElement("span");
+descriptionSpanPrice.innerHTML = `${"€".repeat(eRestaurant.price)}`;
+restaurantDescription.appendChild(descriptionSpanPrice);
+
+//Restaurant map
+const restaurantMap = document.createElement("picture");
+restaurantMap.classList.add("map__img");
+restaurantMap.style.backgroundImage = `url(${eRestaurant.images.map})`
+mainMap.appendChild(restaurantMap);
+    })
+
+    addEventListenerToRestaurant();
+};
+
+fillRestaurant(restaurantList);
+
+
+/* ClICK BUTTON -> ORDER */
+const buttonFilterPrice = document.getElementById("price-button");
+buttonFilterPrice.addEventListener("click", function () {
+  const orderByPrice = restaurantList.sort((e1,e2)=>e1.price-e2.price);
+  fillRestaurant(orderByPrice);
 }
+);
 
-order(restaurantList);
-
-const orderByPrice = restaurantList.sort((e1,e2)=>e1.price-e2.price);
-console.log(orderByPrice);
-
-const orderByPopularity = restaurantList.sort((e1,e2)=>e2.popularity-e1.popularity);
-console.log(orderByPopularity);
-
-const orderByDistance = restaurantList.sort((e1,e2)=>e1.distance-e2.distance);
-console.log(orderByDistance);
-
-/* FUNCTIONS ON CLICK */
-
-
-const resizeableElements = document.querySelectorAll('.resizeable'); // Appelle tous les elements de classe ".resizeable"
+const buttonFilterPopularity = document.getElementById("popularity-button");
+buttonFilterPopularity.addEventListener("click", function() {
+  const orderByPopularity = restaurantList.sort((e1,e2)=>e2.popularity-e1.popularity);
+  fillRestaurant(orderByPopularity);
+}
+);
+const buttonFilterDistance = document.getElementById("distance-button");
+buttonFilterDistance.addEventListener("click", function() {
+  const orderByDistance = restaurantList.sort((e1,e2)=>e1.distance-e2.distance);
+  fillRestaurant(orderByDistance);
+}
+);
 
 
-  let currentlyResized = false; // Déclare qu'aucun élement n'est agrandit
-resizeableElements.forEach((wrapperElement) => { 
-  wrapperElement.addEventListener('click', function () { // clique sur un élement.
-    
-    if (currentlyResized) { // Est-ce qu'il y a un autre élement agrandit ?
-      currentlyResized.classList.remove('large'); // Si oui, réduit cet élement
-      wrapperElement.classList.add('large'); // Et agrandit l'élement sur lequel tu as cliqué
-    }
+function addEventListenerToRestaurant ()
+{
+const wrapper = document.querySelectorAll(".wrapper");
 
-    currentlyResized = wrapperElement; // l'élement sur lequel tu as cliqué, devient un élement agrandit
-  });
-}); 
 
-resizeableElements.forEach((wrapperElement) => {
-    let isFocused = false;
-    let mapElement = wrapperElement.querySelector(".menu__cardMap"); 
-    wrapperElement.addEventListener('click', function () { 
-    
-    if (isFocused) {  // Si l'élement est focus : 
-      wrapperElement.classList.remove('large'); // Reduis le
-      mapElement.classList.add('hidden');
- // Cache la carte
-    } else {
-      wrapperElement.classList.add('large'); // Sinon agrandis le
-      mapElement.classList.remove('hidden');
-   // Montre la carte
-    }
+wrapper.forEach((wrapperElement)=> {
+    let mapElement = wrapperElement.querySelector(".map");
 
-    isFocused = !isFocused;
-
-  });
+    wrapperElement.addEventListener('click', function(){ 
+        
+            wrapper.forEach((el) => {
+                let map = el.querySelector(".map");
+                map.classList.remove('show');
+                el.classList.remove('widden');
+            });
+                mapElement.classList.add('show');
+                wrapperElement.classList.add('widden');
+     }); 
 });
+}
